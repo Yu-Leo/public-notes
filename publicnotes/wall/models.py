2266bb5fb2b27154c5aee016ab644a019fea211c
Lev Yuvenskiy <levayu22@gmail.com>
2022-05-03 07:57:05 +0300

    Edit models

--- publicnotes/wall/models.py
+++ publicnotes/wall/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 class Note(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    content = models.TextField(verbose_name='Текст')
+    content = models.TextField(verbose_name='Текст', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    rating = models.IntegerField(verbose_name='Рейтинг')
-    stared = models.BooleanField(verbose_name='Важная')
-    author = models.ForeignKey('Author', on_delete=models.PROTECT, verbose_name='Автор')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    rating = models.IntegerField(verbose_name='Рейтинг', default=0)
+    stared = models.BooleanField(verbose_name='Важная', default=False)
+    author = models.ForeignKey('Author', on_delete=models.PROTECT, verbose_name='Автор', null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Заметка'
@@ -19,10 +19,13 @@ class Note(models.Model):
 
 
 class Author(models.Model):
-    nickname = models.CharField(max_length=150, verbose_name='Никнейм')
-    email = models.EmailField(verbose_name='Почта')
-    photo = models.ImageField(verbose_name='Аватарка')
-    rating = models.IntegerField(verbose_name='Рейтинг')
+    nickname = models.CharField(max_length=150, verbose_name='Никнейм', unique=True)
+    email = models.EmailField(verbose_name='Почта', unique=True)
+    photo = models.ImageField(verbose_name='Аватарка', blank=True)
+    rating = models.IntegerField(verbose_name='Рейтинг', default=0)
+
+    def __str__(self):
+        return self.nickname
 
     class Meta:
         verbose_name = 'Автор'
@@ -31,7 +34,10 @@ class Author(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Название')
+    title = models.CharField(max_length=150, verbose_name='Название', unique=True)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Категория'
