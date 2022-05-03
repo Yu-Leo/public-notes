526ac3c336def4152c50d786940301b6f6e88aa7
Lev Yuvenskiy <levayu22@gmail.com>
2022-05-03 07:47:54 +0300

    Config admin

--- publicnotes/wall/admin.py
+++ publicnotes/wall/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Note, Author, Category
+
 
 # Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'rating', 'stared', 'author', 'category')
+    list_display_links = ('id', 'title')
+    list_filter = ('created_at', 'author', 'category')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nickname', 'email', 'rating')
+    list_display_links = ('id', 'nickname')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
--- publicnotes/wall/models.py
+++ publicnotes/wall/models.py
@@ -4,21 +4,36 @@ from django.db import models
 # Create your models here.
 
 class Note(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
-    stared = models.BooleanField()
-    author = models.ForeignKey('Author', on_delete=models.PROTECT)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    title = models.CharField(max_length=150, verbose_name='Название')
+    content = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    rating = models.IntegerField(verbose_name='Рейтинг')
+    stared = models.BooleanField(verbose_name='Важная')
+    author = models.ForeignKey('Author', on_delete=models.PROTECT, verbose_name='Автор')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
+        ordering = ['-created_at', 'title']
 
 
 class Author(models.Model):
-    nickname = models.CharField(max_length=150)
-    email = models.EmailField()
-    photo = models.ImageField()
-    rating = models.IntegerField()
+    nickname = models.CharField(max_length=150, verbose_name='Никнейм')
+    email = models.EmailField(verbose_name='Почта')
+    photo = models.ImageField(verbose_name='Аватарка')
+    rating = models.IntegerField(verbose_name='Рейтинг')
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['nickname']
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
