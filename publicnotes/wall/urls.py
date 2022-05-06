ca34f0adfa99f27b196559cc25aa396ef8e3e15b
Lev Yuvenskiy <levayu22@gmail.com>
2022-05-06 07:56:45 +0300

    Add simple 'authors' page

--- publicnotes/wall/urls.py
+++ publicnotes/wall/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('note/<int:pk>/', ViewNote.as_view(), name='note'),
     path('random_note/', random_note, name='random_note'),
     path('category/<int:pk>', ViewCategory.as_view(), name='category'),
-    path('add_note/', add_note, name='add_note')
+    path('add_note/', add_note, name='add_note'),
+    path('authors/', ViewAuthors.as_view(), name='authors'),
 ]
 
 if settings.DEBUG:
