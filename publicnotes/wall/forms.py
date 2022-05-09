8656f39c9c740053f9a4ca9eb73fc8aeb596760f
Lev Yuvensky <levayu22@gmail.com>
2022-05-09 08:22:50 +0300

    Add register form

--- publicnotes/wall/forms.py
+++ publicnotes/wall/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Note, Author
 
@@ -35,3 +37,18 @@ class AuthorForm(forms.ModelForm):
         if ' ' in nickname:
             raise ValidationError('Никнейм не должен содержать пробелов')
         return nickname
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Никнейм",
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.CharField(label="E-mail",
+                            widget=forms.EmailInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(label="Пароль",
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(label="Подтверждение пароля",
+                                widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
