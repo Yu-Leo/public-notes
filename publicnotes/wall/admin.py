756b01d9dae4f1ee9df0666b54dac67f7997b69e
Lev Yuvensky <levayu22@gmail.com>
2022-05-11 17:58:04 +0300

    Customize 'user' in admin

--- publicnotes/wall/admin.py
+++ publicnotes/wall/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 # from .models import Note, Author, Category
+from django.utils.safestring import mark_safe
+
 from .models import Note, Category, User
 from django.contrib.auth.forms import UserChangeForm
 
@@ -13,17 +15,28 @@ class NoteAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'category')
 
 
-class MyUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = User
-
-
 class MyUserAdmin(UserAdmin):
-    form = MyUserChangeForm
+    model = User
 
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('rating',)}),
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Персональная информация', {'fields': ('first_name', 'last_name',
+                                                'email', 'rating', 'photo', 'get_photo', 'bio')}),
+        ('Права доступа', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Даты', {'fields': ('last_login', 'date_joined')}),
     )
+
+    readonly_fields = ('get_photo',)
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return '-'
+
+    get_photo.short_description = 'Текущая аватарка'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -33,5 +46,4 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(User, MyUserAdmin)
 
 admin.site.register(Note, NoteAdmin)
-# admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category, CategoryAdmin)
