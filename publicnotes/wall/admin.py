c546ebdcc6b785e1c66724b8be422a6070440665
Lev Yuvensky <levayu22@gmail.com>
2022-05-11 07:21:50 +0300

    Remove 'Author' model

--- publicnotes/wall/admin.py
+++ publicnotes/wall/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
-from .models import Note, Author, Category
+# from .models import Note, Author, Category
+from .models import Note, Category
 
 
 # Register your models here.
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'rating', 'stared', 'author', 'category')
+    list_display = ('id', 'title', 'created_at', 'rating', 'stared', 'category')
     list_display_links = ('id', 'title')
-    list_filter = ('created_at', 'author', 'category')
+    list_filter = ('created_at', 'category')
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nickname', 'email', 'rating')
-    list_display_links = ('id', 'nickname')
+#
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'nickname', 'email', 'rating')
+#     list_display_links = ('id', 'nickname')
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -20,5 +22,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Note, NoteAdmin)
-admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category, CategoryAdmin)
--- publicnotes/wall/models.py
+++ publicnotes/wall/models.py
@@ -10,7 +10,7 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     rating = models.IntegerField(verbose_name='Рейтинг', default=0)
     stared = models.BooleanField(verbose_name='Важная', default=False)
-    author = models.ForeignKey('Author', on_delete=models.PROTECT, verbose_name='Автор', null=True, blank=True)
+    # author = models.ForeignKey('Author', on_delete=models.PROTECT, verbose_name='Автор', null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория', null=True, blank=True)
 
     def get_absolute_url(self):
@@ -22,23 +22,23 @@ class Note(models.Model):
         ordering = ['-created_at', 'title']
 
 
-class Author(models.Model):
-    nickname = models.CharField(max_length=150, verbose_name='Никнейм', unique=True)
-    email = models.EmailField(verbose_name='Почта', unique=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Аватарка', blank=True)
-    rating = models.IntegerField(verbose_name='Рейтинг', default=0)
-    bio = models.TextField(verbose_name='О себе', blank=True)
-
-    def get_absolute_url(self):
-        return reverse('author', kwargs={"pk": self.pk})
-
-    def __str__(self):
-        return self.nickname
-
-    class Meta:
-        verbose_name = 'Автор'
-        verbose_name_plural = 'Авторы'
-        ordering = ['-rating']
+# class Author(models.Model):
+#     nickname = models.CharField(max_length=150, verbose_name='Никнейм', unique=True)
+#     email = models.EmailField(verbose_name='Почта', unique=True)
+#     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Аватарка', blank=True)
+#     rating = models.IntegerField(verbose_name='Рейтинг', default=0)
+#     bio = models.TextField(verbose_name='О себе', blank=True)
+#
+#     def get_absolute_url(self):
+#         return reverse('author', kwargs={"pk": self.pk})
+#
+#     def __str__(self):
+#         return self.nickname
+#
+#     class Meta:
+#         verbose_name = 'Автор'
+#         verbose_name_plural = 'Авторы'
+#         ordering = ['-rating']
 
 
 class Category(models.Model):
