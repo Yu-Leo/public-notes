c0882ea81bb465f2be3588364948b02264884649
Lev Yuvensky <levayu22@gmail.com>
2022-05-29 09:21:27 +0300

    Refactoring.
    - Refactor imports
    - Add comments
    - Del unused functions

--- publicnotes/wall/urls.py
+++ publicnotes/wall/urls.py
@@ -1,27 +1,28 @@
+from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('note/<int:pk>/', ViewNote.as_view(), name='note'),
-    path('random_note/', random_note, name='random_note'),
-    path('category/<int:pk>/', ViewCategory.as_view(), name='category'),
-    path('add_note/', add_note, name='add_note'),
-    path('authors/', ViewAuthors.as_view(), name='authors'),
-    path('author/<int:pk>/', ViewAuthor.as_view(), name='author'),
-    path('registration/', registration, name='registration'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('about/', About.as_view(), name='about'),
-    path('edit_profile/', edit_profile, name='edit_profile'),
-    path('delete_profile/', delete_profile, name='delete_profile'),
-    path('categories_list/', ViewCategoriesList.as_view(), name='categories_list'),
-    path('edit_note/<int:pk>/', edit_note, name='edit_note'),
-    path('delete_note/<int:pk>/', delete_note, name='delete_note'),
-    path('change_password/', change_password, name='change_password'),
-    path('search/', Search.as_view(), name='search')
+    path('', views.index, name='home'),
+    path('note/<int:pk>/', views.ViewNote.as_view(), name='note'),
+    path('random_note/', views.random_note, name='random_note'),
+    path('category/<int:pk>/', views.ViewCategory.as_view(), name='category'),
+    path('add_note/', views.add_note, name='add_note'),
+    path('authors/', views.ViewAuthors.as_view(), name='authors'),
+    path('author/<int:pk>/', views.ViewAuthor.as_view(), name='author'),
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('about/', views.About.as_view(), name='about'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('delete_profile/', views.delete_profile, name='delete_profile'),
+    path('categories_list/', views.ViewCategoriesList.as_view(), name='categories_list'),
+    path('edit_note/<int:pk>/', views.edit_note, name='edit_note'),
+    path('delete_note/<int:pk>/', views.delete_note, name='delete_note'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('search/', views.Search.as_view(), name='search')
 ]
 
 if settings.DEBUG:
