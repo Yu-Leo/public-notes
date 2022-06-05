489a0af5c0a1a5b39f32c52fe62c4cb314cde17d
Lev Yuvenskiy <levayu22@gmail.com>
2022-06-05 10:31:33 +0300

    Add sending user activation emails

---
+++ publicnotes/wall/utils.py
@@ -0,0 +1,21 @@
+import six
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.sites.shortcuts import get_current_site
+
+from . import models
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user: models.User, timestamp):
+        return six.text_type(user.pk) + six.text_type(timestamp) + \
+               six.text_type(user.is_active)
+
+
+account_activation_token = TokenGenerator()
+
+
+def get_current_domain(request):
+    """
+    :return: current site domain by request object
+    """
+    return get_current_site(request).domain
