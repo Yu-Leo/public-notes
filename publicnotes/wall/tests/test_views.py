57feb220f9dd83ba366316b8bdc8a0b75eb3bb87
Lev Yuvenskiy <levayu22@gmail.com>
2022-06-11 17:44:18 +0300

    Refactor tests for views

--- publicnotes/wall/tests/test_views.py
+++ publicnotes/wall/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from wall.models import Note, User, Category
+from wall.models import Note, User
 
 
 class IndexViewTestCase(TestCase):
@@ -12,14 +12,9 @@ class IndexViewTestCase(TestCase):
                                          email='user_1@localhost',
                                          password='12345')
 
-        cls.user_2 = User.objects.create(username='user_2',
-                                         email='user_2@localhost',
-                                         password='123')
         for i in range(9):
             Note.objects.create(title=f'Note_{i + 1}', is_public=True)
         Note.objects.create(title='Note_11', author=cls.user_1)
-
-        cls.category_1 = Category.objects.create(title='Category 1')
 
     def test_index(self):
         response = self.client.get(reverse('home'))
@@ -30,30 +25,62 @@ class IndexViewTestCase(TestCase):
         response = self.client.get(reverse('home') + '?page=2')
         self.assertEqual(len(response.context['page_obj']), 4)
 
+
+class ViewNoteTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_1 = User.objects.create(username='user_1',
+                                         email='user_1@localhost',
+                                         password='12345')
+
+        cls.user_2 = User.objects.create(username='user_2',
+                                         email='user_2@localhost',
+                                         password='123')
+
+        Note.objects.create(title='Note_1', is_public=True)
+        Note.objects.create(title='Note_11', author=cls.user_1)
+
     def test_view_note_for_existing_note_pk(self):
         response = self.client.get(reverse('note', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wall/note.html')
 
     def test_view_note_for_not_existing_note_pk(self):
-        response = self.client.get(reverse('note', kwargs={'pk': 100}))
+        response = self.client.get(reverse('note', kwargs={'pk': 999}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wall/404.html')
 
     def test_view_note_for_authenticated_author(self):
         self.client.force_login(self.user_1)
-        response = self.client.get(reverse('note', kwargs={'pk': 10}))
+        response = self.client.get(reverse('note', kwargs={'pk': 2}))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'wall/note.html')
 
     def test_view_note_for_authenticated_not_author(self):
         self.client.force_login(self.user_2)
-        response = self.client.get(reverse('note', kwargs={'pk': 10}))
+        response = self.client.get(reverse('note', kwargs={'pk': 2}))
         self.assertRedirects(response, reverse('login'))
 
     def test_view_note_for_anonymous_user(self):
-        response = self.client.get(reverse('note', kwargs={'pk': 10}))
+        response = self.client.get(reverse('note', kwargs={'pk': 2}))
         self.assertRedirects(response, reverse('login'))
+
+
+class RandomNoteViewTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_1 = User.objects.create(username='user_1',
+                                         email='user_1@localhost',
+                                         password='12345')
+
+        cls.user_2 = User.objects.create(username='user_2',
+                                         email='user_2@localhost',
+                                         password='123')
+
+        Note.objects.create(title='Note_1', is_public=True)
+        Note.objects.create(title='Note_2', author=cls.user_1)
 
     def test_random_note_when_notes_exist(self):
         response = self.client.get(reverse('random_note'))
@@ -63,6 +90,22 @@ class IndexViewTestCase(TestCase):
         Note.objects.all().delete()  # Delete all notes (needs for this test)
         response = self.client.get(reverse('random_note'))
         self.assertRedirects(response, reverse('home'))
+
+
+class AddNoteViewTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_1 = User.objects.create(username='user_1',
+                                         email='user_1@localhost',
+                                         password='12345')
+
+        cls.user_2 = User.objects.create(username='user_2',
+                                         email='user_2@localhost',
+                                         password='123')
+
+        Note.objects.create(title='Note_1', is_public=True)
+        Note.objects.create(title='Note_2', author=cls.user_1)
 
     def test_add_note_get(self):
         self.client.force_login(self.user_1)
@@ -80,7 +123,7 @@ class IndexViewTestCase(TestCase):
             'is_pined': False,
         }
         response = self.client.post(reverse('add_note'), data=data)
-        self.assertRedirects(response, reverse('note', kwargs={'pk': 11}))
+        self.assertRedirects(response, reverse('note', kwargs={'pk': 3}))
 
     def test_add_note_get_with_correct_category_pk(self):
         self.client.force_login(self.user_1)
